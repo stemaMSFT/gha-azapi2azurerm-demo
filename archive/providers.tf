42b7a75f03be07163aa115806d00d9e16ab9683d
Steven Ma <stema@microsoft.com>
2023-08-03 15:21:35 -0700

    Simplify example

---
+++ archive/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+    }
+
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "tfstate"
+    storage_account_name = "tfstate8q1ou"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
+}
+
+provider "azapi" {
+}
+
+provider "azurerm" {
+  features {}
+}
+
