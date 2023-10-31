b8f3218eee29a930a1d63d297e6f97411befb04e
Steven Ma <stema@microsoft.com>
2023-10-31 14:50:51 -0700

    Attempting to test apply functionality

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # __generated__ by Terraform from "/subscriptions/6e4d4d41-20a6-4801-8eec-c769d870e333/resourceGroups/tfstate"
-resource "azapi_resource" "example" {
+resource "azapi_resource" "qs101" {
   body                      = "{\"properties\":{}}"
   ignore_casing             = false
   ignore_missing_property   = true
@@ -19,28 +19,28 @@ resource "azapi_resource" "example" {
     update = null
   }
 }
-# # Provision a Lab Service Account and a Lab that are in public preview
-# resource "azapi_resource" "qs101-account" {
-#   type      = "Microsoft.LabServices/labaccounts@2018-10-15"
-#   name      = "qs101LabAccount"
-#   parent_id = azurerm_resource_group.qs101.id
+# Provision a Lab Service Account and a Lab that are in public preview
+resource "azapi_resource" "qs101-account" {
+  type      = "Microsoft.LabServices/labaccounts@2018-10-15"
+  name      = "qs101LabAccount"
+  parent_id = azurerm_resource_group.qs101.id
 
-#   body = jsonencode({
-#     properties = {
-#       enabledRegionSelection = false
-#     }
-#   })
-# }
+  body = jsonencode({
+    properties = {
+      enabledRegionSelection = false
+    }
+  })
+}
 
-# resource "azapi_resource" "qs101-lab" {
-#   type      = "Microsoft.LabServices/labaccounts/labs@2018-10-15"
-#   name      = "qs101Lab"
-#   parent_id = azapi_resource.qs101-account.id
+resource "azapi_resource" "qs101-lab" {
+  type      = "Microsoft.LabServices/labaccounts/labs@2018-10-15"
+  name      = "qs101Lab"
+  parent_id = azapi_resource.qs101-account.id
 
-#   body = jsonencode({
-#     properties = {
-#       maxUsersInLab  = 10
-#       userAccessMode = "Restricted"
-#     }
-#   })
-# }
+  body = jsonencode({
+    properties = {
+      maxUsersInLab  = 10
+      userAccessMode = "Restricted"
+    }
+  })
+}
